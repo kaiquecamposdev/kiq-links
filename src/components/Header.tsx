b7d2e09ac0a3595b58dcc841fa57a92861aefa46
kaique <kaiquedecampos2@gmail.com>
2023-10-14 00:33:41 -0300

    fix: readjustment of paths

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,10 @@
+import Perfil from '@/assets/perfil.png'
+
 export function Header() {
   return (
     <header className="flex flex-col items-center gap-2 p-6">
       <img
-        src="/src/assets/perfil.png"
+        src={Perfil}
         alt=""
         className="max-w-28 max-h-28 rounded-full border-[2px] border-zinc-400 transition-transform hover:scale-[1.1]"
       />
